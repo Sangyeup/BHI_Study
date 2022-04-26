dc906d6de672a78e24694c8919d689f60391f8bf
dohyeon-scott-kim <dohyeon.scott.kim@gmail.com>
2022-04-26 10:13:51 +0900

    dp3 done by dohyomi

---
+++ Dynamic_Programming/Problem3/problem3_dohyomi.py
@@ -0,0 +1,76 @@
+import time
+import numpy as np
+"""
+Read the input from input.txt
+Write your answer to output.txt
+"""
+
+input_f = open("input.txt", 'r')
+lines = input_f.readlines()
+input_f.close()
+output_f = open("output_dohyomi.txt", 'w')
+max_num = 1000000
+
+start = time.time()
+# Iterate each test case in input.txt
+for test_case in range(0, 40, 4):
+    n = int(lines[test_case])    # n is the length of string
+    s = []
+    s.append(list(map(int, lines[test_case+1].rstrip('\n').split(' '))))  # s[0] is the first row of table
+    s.append(list(map(int, lines[test_case+2].rstrip('\n').split(' '))))  # s[1] is the second row of table
+    s.append(list(map(int, lines[test_case+3].rstrip('\n').split(' '))))  # s[2] is the second row of table
+    
+    s = np.array(s).T
+
+    # print(s)
+    Answer = 0
+    
+    """
+    YOUR CODE HERE 
+    Save your answer to the list variable Answer.
+    """
+    
+    p = [
+        [1, -1, 0],
+        [1, 0, -1],
+        [-1, 1, 0],
+        [-1, 0, 1],
+        [0, 1, -1], 
+        [0, -1, 1]
+    ]
+    p = np.array(p)
+
+    w = [[0] * 6 for i in range(n)]
+    score = [[0] * 6 for i in range(n)]
+
+    for i in range(n):
+        for j in range(len(p)):
+            w[i][j] = np.sum(s[i] * p[j])
+
+    for i in range(len(p)):
+        score[0][i] = w[0][i]
+
+    for i in range(1, n):
+        for j in range(len(p)):
+            if j == 0:
+                score[i][j] = max(score[i-1][3], score[i-1][4]) + w[i][j]
+            elif j == 1:
+                score[i][j] = max(score[i-1][2], score[i-1][5]) + w[i][j]
+            elif j == 2:
+                score[i][j] = max(score[i-1][1], score[i-1][5]) + w[i][j]
+            elif j == 3:
+                score[i][j] = max(score[i-1][0], score[i-1][5]) + w[i][j]
+            elif j == 4:
+                score[i][j] = max(score[i-1][0], score[i-1][3]) + w[i][j]
+            elif j == 5:
+                score[i][j] = max(score[i-1][1], score[i-1][2]) + w[i][j]
+    
+    Answer = max(score[n-1])
+    # print(Answer)
+    # print(w[0])
+    # Print the answer to output.txt
+    output_f.write("#" + str(int(test_case/4 + 1)) + " " + str(Answer) + "\n")
+    s = []
+end = time.time()
+output_f.write(f"{end - start:.5f} sec")
+output_f.close()
